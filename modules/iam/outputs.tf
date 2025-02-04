7ddb0c0cc0a6dcfa67e449cd5911d23dff779268
Jeremy Brown <chase208@4JLT.com>
2025-02-03 21:50:50 -0500

    Fixing role arn name

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,4 +1,4 @@
-output "apprunner_role_arn" {
+output "apprunner_execution_role" {
     description = "The arn of the apprunner execution role"
     value       = aws_iam_role.apprunner_execution_role.arn
 }
